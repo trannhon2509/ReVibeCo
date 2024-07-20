8e4ff9a4b0d4e7501196eeab5bc7151842ec6451
Nhon Tran <nhonvn2509@gmail.com>
2024-07-20 10:48:17 +0700

    chore: Remove commented code for Clients component in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 import Deals from '../components/Deals'
 import About from '../components/About'
 import Products from '../components/Products'
-import Clients from '../components/Clients'
+// import Clients from '../components/Clients'
 import Footer from '../components/Footer'
 
 function Home() {
